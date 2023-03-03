4af411c157f48c8095df4e30db65c13596920adb
tahapek5454 <tahapek5454@gmail.com>
2023-03-03 20:49:54 +0300

    feat: add dbContext

---
+++ selfWaiterBackend/Persistence/Contexts/SelfWaiterDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class SelfWaiterDbContext : DbContext
+    {
+        public SelfWaiterDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
